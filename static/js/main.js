198cf60470893961400136436e0995de4ff11109
MamurTurdikulov <148514012+MamurTurdikulov@users.noreply.github.com>
2025-12-09 07:54:12 +0300

    Delete static directory

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("submit", function(e) {
-    const numberInput = e.target.querySelector("input[type=number]");
-    if (numberInput) {
-        if (parseInt(numberInput.value) <= 0) {
-            e.preventDefault();
-            alert("Amount must be at least 1 kg.");
-        }
-    }
-});
